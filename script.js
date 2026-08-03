4bd0cae52d98f962ea31916d3dfa314446757436
Anonymous <anonymous4366@users.noreply.github.com>
2026-08-04 01:17:47 +0800

    Refine project page presentation

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const videos = [...document.querySelectorAll('video')];
 videos.forEach((video) => {
   video.addEventListener('play', () => {
     videos.forEach((other) => {
-      if (other !== video) other.pause();
+      const sameSyncGroup = video.dataset.syncGroup
+        && video.dataset.syncGroup === other.dataset.syncGroup;
+
+      if (other !== video && !sameSyncGroup) other.pause();
     });
   });
 });
